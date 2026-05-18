ce64d45481fffade0259504df850e0a43144b4bf
Rafael <rafaelrosale81@gmail.com>
2026-05-18 18:06:22 -0400

    [FTR] - add github workfows

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:4200',
+    defaultCommandTimeout: 30000,
+    setupNodeEvents(on, config) {;
+      const env = config.env['ENVIRONMENT'] || 'dev';
+      config.env['currentEnv'] = env;
+      return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  },
+  video: true,
+  videoCompression: false,
+  screenshotOnRunFailure: true,
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts',
   },
 });
